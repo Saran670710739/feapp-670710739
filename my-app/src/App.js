5f8116bb133dc3ca2661b43a8bfbc025bf5d24cc
Saran Watchawong <watchawong_s@silpakorn.edu>
2026-08-27 10:30:52 +0000

    labweek6

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import About from './pages/About';
+import MovieDetail from './pages/MovieDetail';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <div className="flex min-h-screen flex-col bg-slate-50">
+      <Navbar />
+
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movie />} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
